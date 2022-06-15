370b721516055001224139e9daf90035ecfb7eb1
God Hand <interstellar300000000@gmail.com>
2022-06-15 12:46:28 +0100

    A code that passes the betty test

---
+++ betty_style.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+**/
+* betty style code for ALX SE project
+* A code that passes the simple Betty check
+* Return: 0
+*/
+
+Int main(void)
+
+{ printf("A code that passes Betty check");
+
+return 0;
+}
+
